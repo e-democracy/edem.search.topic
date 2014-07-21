defde1bc1c030e1e28c9df0488aef439792b8de6
Bill Bushey <wbushey@gmail.com>
2014-07-21 15:17:31 -0500

    Added minified version of js

---
+++ edem/search/topic/browser/javascript/.js$:-min.js
@@ -0,0 +1,4 @@
+"use strict";jQuery.noConflict();function gs_search_topics_site_homepage(){var b="#gs-search-topic-site-home-search",a=null;
+a=GSSearch(b,"/s/search.ajax",0,10,{m:"1"},null);jQuery("#gs-site-home-activity-tabs li:first-child a").click();
+a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_search_topics_site_homepage)
+});
